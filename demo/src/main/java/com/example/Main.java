093af435ffb56eaea6e1fd18041d9888cf7cfcfd
Marco Balzter <marco.balzter@gmail.com>
2023-12-22 15:12:33 +0100

    Update Main.java

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,6 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello you wonderful World.");
-        System.out.println("--------------------------");
+        System.out.println("-");
     }
 }
